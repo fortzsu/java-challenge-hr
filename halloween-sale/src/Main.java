a8044d225e1379de2ecaa3d87fc31bf4c5b23fa4
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-09-20 13:45:41 +0200

    add halloween-sale - refact

--- halloween-sale/src/Main.java
+++ halloween-sale/src/Main.java
@@ -3,9 +3,6 @@ public class Main {
         System.out.println(howManyGames(20, 3, 6, 70)); //5
         System.out.println(howManyGames(20, 3, 6, 80)); //6
         System.out.println(howManyGames(20, 3, 6, 85)); //7
-        System.out.println(howManyGames(100, 1, 1, 99)); //0
-        System.out.println(howManyGames(100, 12, 55, 95)); //0
-        System.out.println(howManyGames(100, 19, 1, 180)); //1
     }
 
     public static int howManyGames(int p, int d, int m, int s) {
