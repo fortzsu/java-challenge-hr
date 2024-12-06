30b55c32f07434aeee7eab25a58870a0f2dc892e
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-12-06 18:51:07 +0100

    add chocolate-feast

--- chocolate-feast/src/Main.java
+++ chocolate-feast/src/Main.java
@@ -1,14 +1,21 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println(chocolateFeast(12, 4, 4)); //3
-        System.out.println(chocolateFeast(10, 2, 5)); //6
         System.out.println(chocolateFeast(15, 3, 2)); //9
         System.out.println(chocolateFeast(7, 3, 2)); //3
+        System.out.println(chocolateFeast(21816, 14645, 1827)); //1
         System.out.println(chocolateFeast(6, 2, 2)); //5
+        System.out.println(chocolateFeast(16809, 123, 11668)); //136
+        System.out.println(chocolateFeast(1575, 1, 262)); //1581
     }
 
     public static int chocolateFeast(int n, int c, int m) {
-
-        return 0;
+        n = n / c;
+        int result = n;
+        while (n >= m) {
+            result = result + (n / m);
+            n = (n / m) + (n % m);
+        }
+        return result;
     }
 }
